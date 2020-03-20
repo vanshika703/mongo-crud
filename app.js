1a5ce16f87170aa9641ff59cb1816866c741ed0e
vanshika703 <vanshikabhojnagarwala@gmail.com>
2020-03-20 16:06:25 +0530

    1st try

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.set("view engine","ejs")
+
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb+srv://vanshika703:vanshika@cluster0-ndrdb.mongodb.net/test?retryWrites=true&w=majority"
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+app.post('/', (req,res)=>{
+    MongoClient.connect(url,(err,db)=>{
+        if(err) throw err
+        let dbo = db.db("forms")
+        var input = {
+            name : req.body.name,
+            gender : req.body.gender
+        }
+        dbo.collection('inputs').insertOne(input, (dbErr,res)=>{
+            if(dbErr) throw dbErr
+            console.log("inserted input")
+        })
+    })
+})
+
+
+app.listen(3000, (err)=>{
+    if(err) throw err
+
+    console.log("app listening on 3000")
+})
